3055c34150ea98508bbfc9f595b1c69747aac722
Jorge Olivares <jlolivaresp@gmail.com>
2017-07-21 11:37:26 -0400

    Update function

--- fault_detector_F.py
+++ fault_detector_F.py
@@ -1,41 +1,60 @@
-def fault_detector_F(lista,delta_var,conf_lev):
+"""
+Funcion que detecta cambios en la varianza en un grupo de muestras de tamano N en base a la prueba estadistica F-test.
+Para ello se va comparando una ventana movil de tamano N con el conjunto de valores que anteceden temporalmente dicha
+ventana.
+
+Parametros de entrada:
+
+lista: Vector de dimensiones 1 x N sobre el cual se hara la prueba
+delta_var: Variacion de la varianza que se quisiera detectar como limite
+conf_lev: Nivel de confiabilidad del 0 - 1 que se quiere tener para la clasificacion de los resultados
+N: Numero de muestras a ser usadas como ventana de prueba. Si es 'auto', la funcion calcula automaticamente el numero de
+    muestras necesarias para dectear dicho cambio en base a conf_lev y stand_dv.
+
+Parametros de salida:
+
+lista_falla: valores del vector lista para los cuales se detecto variacion en la varianza
+falla_bool: Vector de longitud 1 x N de 0 y 1 que coincide con lista de modo que para cada posicion se asigana un 1 si
+            para la misma posicion en lista el valor es considerado anomalo o un 0 en caso contrario
+fault_counter: Numero de valores anomalos detectados
+N: Tamano de la ventana de muestras sobre los cuales se aplico F-test. En caso de que se requiera saber cual fue el
+    valor calculado en modo N = 'auto'
+
+"""
+
+
+def fault_detector_F(lista, delta_var, conf_lev, N = 'auto'):
+
+    import scipy.stats
+    import scipy as sp
     import numpy as np
-    import scipy.stats as sp
 
-    # Calculamos el tamano N de la muestra para detectar un cambio en la varianza delta_var
+    falla_bool = np.zeros(len(lista))
+    if np.std(lista) > 0.0001:
+        if N == 'auto':
+            N = 1
+            X_2_table = sp.stats.chi2.ppf(q=conf_lev,df=N)
+            X_2 = ((lista.std()+delta_var)/np.std(lista))**2
 
-    if np.std(lista) > 0.001:
-        N = 1
-        X_2_table = sp.chi2.ppf(q=conf_lev,df=N)
-        X_2 = ((lista.std()+delta_var)/np.std(lista))**2
+            while X_2 < X_2_table:
+                N += 1
+                X_2_table = sp.stats.chi2.ppf(q=conf_lev,df=N)
+                X_2 = (N - 1)*(((lista.std()+delta_var)/np.std(lista))**2)
+                if N > len(lista)/2:
+                    print('No se cuenta con suficientes valores para detectar un '
+                          'cambio en la varianza de {}, pruebe indicando el valor de N'.format(delta_var))
+                    break
 
-        while X_2 < X_2_table:
-            N += 1
-            X_2_table = sp.chi2.ppf(q=conf_lev,df=N)
-            X_2 = (N - 1)*(((lista.std()+delta_var)/np.std(lista))**2)
-            if N > len(lista)/2:
-                break
-
-        # Iteramos a lo largo de los datos para comprobar si hubo o no un cambio en la varianza
-
-        fault_counter = 0
         cont = 0
-        t_falla = np.array([])
-        while N+cont <= len(lista)-N:
+        while N+cont <= len(lista) - N:
             dfn = len(lista[:N+cont])
-            if np.std(lista[N+cont:2*N+cont]) > 0.001:
+            if np.std(lista[N+cont:2*N+cont]) > 0.0001:
                 F_N1_N2 = (np.std(lista[:N+cont])/np.std(lista[N+cont:2*N+cont]))**2
-                F_table = sp.f.ppf(q=conf_lev,dfn=dfn,dfd=N)
+                F_table = sp.stats.f.ppf(q=conf_lev, dfn=dfn, dfd=N)
 
-                if F_N1_N2 > F_table:
-                    t_falla = np.append(t_falla,lista[N+cont:2*N+cont].index[0])
-                    fault_counter += 1
-                cont += 1
-            else:
-                t_falla = [0,0]
-                break
-    else:
-        t_falla = [0,0]
-    inicio_falla = t_falla[0]
-    fin_falla = t_falla[-1]
-    return inicio_falla,fin_falla
+                if F_N1_N2 < F_table:
+                    falla_bool[N+cont:2*N+cont] = np.ones(N)
+            cont += 1
+    fault_counter = len(falla_bool[falla_bool == 1])
+    lista_falla = lista[falla_bool == 1]
+    return lista_falla, falla_bool, fault_counter, N
--- fault_detector_ttest.py
+++ fault_detector_ttest.py
@@ -17,6 +17,9 @@ Parametros de salida:
 lista_falla: valores del vector lista para los cuales se detecto variacion en la media
 falla_bool: Vector de longitud 1 x N de 0 y 1 que coincide con lista de modo que para cada posicion se asigana un 1 si
             para la misma posicion en lista el valor es considerado anomalo o un 0 en caso contrario
+fault_counter: Numero de valores anomalos detectados
+N: Tamano de la ventana de muestras sobre los cuales se aplico t-test. En caso de que se requiera saber cual fue el
+    valor calculado en modo N = 'auto'
 
 """
 
@@ -31,17 +34,16 @@ def fault_detector_ttest(lista, stand_dv, conf_lev, delta_mean, N = 'auto'):
         N = ceil((stand_dv*sp.stats.norm.ppf((1+conf_lev)/2)/delta_mean)**2)
     falla_bool = np.zeros(len(lista))
     if 2*N <= len(lista):
-        fault_counter = 0
         cont = 0
-        while N+cont <= len(lista):
+        while N+cont <= len(lista) - N:
             ttest_issermann = (lista[:N+cont].mean()-lista[N+cont:2*N+cont].mean())*\
                                 math.sqrt(N*N*(2*N - 2)/(2*N))/math.sqrt((2*N-2)*(stand_dv**2))
             if abs(ttest_issermann) > sp.stats.t.ppf(conf_lev, 2*N-2):
                 falla_bool[N+cont:2*N+cont] = np.ones(N)
-                fault_counter += 1
             cont += 1
     else:
         print('N debe ser menor a la longitud del vector lista')
+    fault_counter = len(falla_bool[falla_bool == 1])
     lista_falla = lista[falla_bool == 1]
-    return lista_falla, falla_bool
+    return lista_falla, falla_bool, fault_counter, N
 
